95d91132722f29e44b721c52c139e9cf478f0a51
Issam Bouhadida <issam.bouhadida@gmail.com>
2021-02-07 16:32:57 +0100

    Add files via upload

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,28 @@
-#Explain your work
+#!/usr/bin/env python
+# coding: utf-8
 
-#Question 1
-for x in range(a):
-	print(a)
+# In[ ]:
+
+
+value1= (input("Please enter Value1:"))
+value2= (input("Please enter Value2:"))
+value3= input("Please enter Value3:")
+value4= input("Please enter Value4:")
+value5= input("Please enter Value5:")
+print(f"value1:{value1}")
+print(f"value2:{value2}")
+print(f"value3:{value3}")
+print(f"value14:{value4}")
+print("value5:{}".format(value5))
+print ("data_type of Value1:",type(value1))
+print ("data_type of Value2:",type(value2))
+print ("data_type of Value3:",type(value3))
+print ("data_type of Value4:",type(value4))
+print ("data_type of Value5:",type(value5))
+
+
+# In[ ]:
+
+
+
+
